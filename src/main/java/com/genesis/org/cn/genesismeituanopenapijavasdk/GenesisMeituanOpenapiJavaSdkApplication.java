ddcd2bcb15a15ff6ce8a54ad2b4e86aec4bfd166
Steven Liu <1012987522@qq.com>
2023-10-16 19:41:16 +0800

    :tada: feature: 初始化项目

---
+++ src/main/java/com/genesis/org/cn/genesismeituanopenapijavasdk/GenesisMeituanOpenapiJavaSdkApplication.java
@@ -0,0 +1,13 @@
+package com.genesis.org.cn.genesismeituanopenapijavasdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenesisMeituanOpenapiJavaSdkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenesisMeituanOpenapiJavaSdkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/genesis/org/cn/genesismeituanopenapijavasdk/GenesisMeituanOpenapiJavaSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genesis.org.cn.genesismeituanopenapijavasdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenesisMeituanOpenapiJavaSdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
