bf7830092a318ac9c3cd6f242d193f19cdf8503e
Steven Liu <1012987522@qq.com>
2024-03-22 16:04:26 +0800

    :sparkles: feat(DTH-10006): 新增门店收银凭证对接Dao层对象.

---
+++ src/main/java/com/genesis/org/cn/genesismeituanopenapijavasdk/dao/api/IKingdeePosBillAuxiliaryCalledDao.java
@@ -0,0 +1,14 @@
+package com.genesis.org.cn.genesismeituanopenapijavasdk.dao.api;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.genesis.org.cn.genesismeituanopenapijavasdk.dao.entity.KingdeePosBillAuxiliaryCalledEntity;
+
+/**
+ * KingdeePosBillAuxiliaryCalled表数据层接口
+ *
+ * @author 人工智能
+ * @date 2024-03-22 15:28:32
+ */
+public interface IKingdeePosBillAuxiliaryCalledDao extends IService<KingdeePosBillAuxiliaryCalledEntity> {
+
+}
---
+++ src/main/java/com/genesis/org/cn/genesismeituanopenapijavasdk/dao/api/impl/JdPosShopBillDaoImpl.java
@@ -0,0 +1,18 @@
+package com.genesis.org.cn.genesismeituanopenapijavasdk.dao.api.impl;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.genesis.org.cn.genesismeituanopenapijavasdk.dao.api.IJdPosShopBillDao;
+import com.genesis.org.cn.genesismeituanopenapijavasdk.dao.entity.JdPosShopBillEntity;
+import com.genesis.org.cn.genesismeituanopenapijavasdk.dao.mapper.JdPosShopBillMapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * JdPosShopBill表数据层实现
+ *
+ * @author 人工智能
+ * @date 2024-03-22 15:28:32
+ */
+@Repository
+public class JdPosShopBillDaoImpl extends IService<JdPosShopBillMapper, JdPosShopBillEntity> implements IJdPosShopBillDao {
+
+}
