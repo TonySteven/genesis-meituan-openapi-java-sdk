39c2f57f58cb218a5b66a3796092d7cf0f09023a
Steven Liu <1012987522@qq.com>
2024-03-22 17:02:04 +0800

    :sparkles: feat(DTH-10006): 更新门店收银凭证对接Dao层对象.

--- src/main/java/com/genesis/org/cn/genesismeituanopenapijavasdk/dao/api/impl/JdPosShopBillDaoImpl.java
+++ src/main/java/com/genesis/org/cn/genesismeituanopenapijavasdk/dao/api/impl/JdPosShopBillDaoImpl.java
@@ -1,6 +1,6 @@
 package com.genesis.org.cn.genesismeituanopenapijavasdk.dao.api.impl;
 
-import com.baomidou.mybatisplus.extension.service.IService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.genesis.org.cn.genesismeituanopenapijavasdk.dao.api.IJdPosShopBillDao;
 import com.genesis.org.cn.genesismeituanopenapijavasdk.dao.entity.JdPosShopBillEntity;
 import com.genesis.org.cn.genesismeituanopenapijavasdk.dao.mapper.JdPosShopBillMapper;
@@ -10,9 +10,9 @@ import org.springframework.stereotype.Repository;
  * JdPosShopBill表数据层实现
  *
  * @author 人工智能
- * @date 2024-03-22 15:28:32
+ * &#064;date  2024-03-22 15:28:32
  */
 @Repository
-public class JdPosShopBillDaoImpl extends IService<JdPosShopBillMapper, JdPosShopBillEntity> implements IJdPosShopBillDao {
+public class JdPosShopBillDaoImpl extends ServiceImpl<JdPosShopBillMapper, JdPosShopBillEntity> implements IJdPosShopBillDao {
 
 }
--- src/main/java/com/genesis/org/cn/genesismeituanopenapijavasdk/dao/api/impl/KingdeePosBillAuxiliaryCalledDaoImpl.java
+++ src/main/java/com/genesis/org/cn/genesismeituanopenapijavasdk/dao/api/impl/KingdeePosBillAuxiliaryCalledDaoImpl.java
@@ -1,6 +1,6 @@
 package com.genesis.org.cn.genesismeituanopenapijavasdk.dao.api.impl;
 
-import com.baomidou.mybatisplus.extension.service.IService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.genesis.org.cn.genesismeituanopenapijavasdk.dao.api.IKingdeePosBillAuxiliaryCalledDao;
 import com.genesis.org.cn.genesismeituanopenapijavasdk.dao.entity.KingdeePosBillAuxiliaryCalledEntity;
 import com.genesis.org.cn.genesismeituanopenapijavasdk.dao.mapper.KingdeePosBillAuxiliaryCalledMapper;
@@ -10,9 +10,9 @@ import org.springframework.stereotype.Repository;
  * KingdeePosBillAuxiliaryCalled表数据层实现
  *
  * @author 人工智能
- * @date 2024-03-22 15:28:32
+ * &#064;date  2024-03-22 15:28:32
  */
 @Repository
-public class KingdeePosBillAuxiliaryCalledDaoImpl extends IService<KingdeePosBillAuxiliaryCalledMapper, KingdeePosBillAuxiliaryCalledEntity> implements IKingdeePosBillAuxiliaryCalledDao {
+public class KingdeePosBillAuxiliaryCalledDaoImpl extends ServiceImpl<KingdeePosBillAuxiliaryCalledMapper, KingdeePosBillAuxiliaryCalledEntity> implements IKingdeePosBillAuxiliaryCalledDao {
 
 }
