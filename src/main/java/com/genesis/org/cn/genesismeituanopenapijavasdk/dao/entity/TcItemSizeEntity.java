f85f3b19d9b68eba7a26053b7945eab7b45c3bf8
李佩 <2296392921@qq.com>
2024-06-02 14:30:13 +0800

    main:解决定时任务主键未生成问题

--- src/main/java/com/genesis/org/cn/genesismeituanopenapijavasdk/dao/entity/TcItemSizeEntity.java
+++ src/main/java/com/genesis/org/cn/genesismeituanopenapijavasdk/dao/entity/TcItemSizeEntity.java
@@ -1,5 +1,6 @@
 package com.genesis.org.cn.genesismeituanopenapijavasdk.dao.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
@@ -32,7 +33,7 @@ public class TcItemSizeEntity implements Serializable {
     /**
      * ID
      */
-    @TableId("id")
+    @TableId(value = "id", type = IdType.ASSIGN_ID)
     private String id;
 
     /**
